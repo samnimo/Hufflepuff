2814630a4b7ce965d0fe059605eba0d434aed2a4
unknown <apostlekwadwo719@gmail.com>
2026-03-21 17:49:51 +0000

    Right angled triangle

---
+++ Asamoah-Justice/Assignments/right_angled_triangle.py
@@ -0,0 +1,19 @@
+#defining/naming the function using def
+def right_angled_triangle(n):
+    #building the right angled triangle using for loop
+    for i in range(1, n+1):
+        #printing the right_angled_triangle
+        print("###" * i)
+
+#calling the function
+
+
+def right_angled_triangle():
+    print("#")
+    print("#" * 2)
+    print("#" * 3)
+    print("#" * 4)
+    print("#" * 5)
+    print("#" * 6)
+    print("#" * 7)
+right_angled_triangle()
