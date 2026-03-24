fe8f2009afcd97bca0d23626c91003624513b2a9
samnimo <samuelnimo94@gmail.com>
2026-03-24 18:39:41 +0000

    THIS IS SAMUEL NIMO

---
+++ samuelnimo/Lecture1/main.py
@@ -0,0 +1,38 @@
+print("Hello  World")
+
+def new_python_code():
+    print("Cooked")
+    
+new_python_code()
+print("finish everything")
+ 
+def cat_sound():
+     print("Mmeeeeeemm")
+     print("Mmeeeeeemm")
+     print("Mmeeeeeemm")
+     
+cat_sound()
+cat_sound()
+
+
+def add_num():
+    print("2 + 2")
+    
+add_num()
+
+
+
+def alert_me():
+    alert = "SOMEONE IS HERE"
+    print(alert)
+    
+alert_me()
+print(alert_me())
+
+
+def alert_me():
+    return "SOMEONE IS HERE"
+    
+    
+alert_me()
+print(alert_me())
