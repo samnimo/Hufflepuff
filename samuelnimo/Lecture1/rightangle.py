0b674d526b32ab3dcb34da3a5607c7a374322bc7
samnimo <samuelnimo94@gmail.com>
2026-03-25 15:55:36 +0000

    Right Angle Triangle assigment

---
+++ samuelnimo/Lecture1/rightangle.py
@@ -0,0 +1,12 @@
+rows = 10
+# Outer loop to handle the rows
+for i in range(rows):
+    
+    # Inner loop to handle the columns
+    for j in range(i + 1):
+        
+        # Printing the pattern
+        print("*", end=' ')
+    
+    # Next Line
+    print()
